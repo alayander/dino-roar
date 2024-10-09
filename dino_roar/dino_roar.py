05c7508a04ceb6bae8ba1ae4c04ffa1dde39e537
Alexander Lay <alexanderqlay@gmail.com>
2024-10-08 22:55:38 -0400

    bot: Implement date parsing for update command
    Signed-off-by: Alexander Lay <alexanderqlay@gmail.com>

--- dino_roar/dino_roar.py
+++ dino_roar/dino_roar.py
@@ -1,5 +1,7 @@
 import os
+import re
 import discord
+from datetime import datetime
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -8,11 +10,19 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='🦖 ', intents=intents)
 
 # Sample command
 @bot.command(name='update')
-async def ping(ctx):
-    await ctx.send('Dino Jump!')
+async def update(ctx, *, input: str = None):
+
+    date = re.match(r'^\d{2}/\d{2}/\d{4}$', input)
+
+    if date:
+        date_obj = datetime.strptime(date.group(), "%m/%d/%Y")
+        formatted_date = date_obj.strftime("%B %d, %Y")
+        await ctx.send(f"**Dino Jump Status Update - {formatted_date}**")
+    else:
+        await ctx.send(f"Invalid `[date]` readback - expecting format mm/dd/yyyy")
 
 bot.run(DISCORD_TOKEN)
