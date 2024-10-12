049ec1810e9e4017deba888daf89e0d55c625a72
Alexander Lay <alexanderqlay@gmail.com>
2024-10-11 22:13:48 -0400

    bot: Extract weekly update dict from Confluence
    Signed-off-by: Alexander Lay <alexanderqlay@gmail.com>

--- dino_roar/confluence.py
+++ dino_roar/confluence.py
@@ -3,7 +3,6 @@ This file contains functions used to fetch status update from Confluence.
 """
 
 import os
-import re
 import requests
 from bs4 import BeautifulSoup
 from requests.auth import HTTPBasicAuth
@@ -15,49 +14,62 @@ USERNAME = os.getenv("ATLASSIAN_USERNAME")
 API_TOKEN = os.getenv("ATLASSIAN_TOKEN")
 
 SUBTEAMS = ["Camera", "Game", "ECE Display", "Mech Display"]
-UPDATE_CATEGORIES = ["LWG", "R", "TWG"]
+UPDATE_CATEGORIES = ["R:", "TWG:"]
 
 
-def form_update_dict(raw_update: str):
+def extract_list_items(ul_element):
     """
-    Returns a dictionary after parsing the raw string from the Confluence page scrape
+    Recursively extract list items given a unordered list element.
 
     Args:
-        raw_update (str): Status update string scraped from a web page.
+        ul_element (Tag): Unordered list element to extract from
 
     Returns:
-        dict: Dictionary broken down into status update keys.
+        list: List of elements and subelements
     """
-    update_dict = {}
-    soup = BeautifulSoup(raw_update, "html.parser")
+    extracted_items = []
+    for item in ul_element.find_all("li", recursive=False):
+        item_text = item.p.get_text(strip=True) if item.p else item.get_text(strip=True)
+        next_level = item.find("ul")
+        if next_level:
+            extracted_items.append({item_text: extract_list_items(next_level)})
+        else:
+            extracted_items.append(item_text)
+    return extracted_items
 
-    date = soup.find("h2").text
-    update_dict[date] = {}
 
-    # TODO - Iterate through soup to form dictionary
-
-    return update_dict
-
-
-def parse_content(content: str, date: str):
+def parse_content(raw_content: str, date: str):
     """
-    Parses a raw string from status update page for the substring for the specified date
+    Parses a raw string from status update page for the substring for the specified date.
 
     Args:
-        content (str): Raw string of contents scraped from the Confluence web page.
+        raw_content (str): Raw string of contents scraped from the Confluence web page.
         date (str): The date of the status update to be parsed for.
 
     Returns:
         dict: Dictionary broken down into status update keys.
     """
-    # TODO - adjust regex pattern
-    weekly_update_pattern = re.compile(
-        rf"<h2>{date}.*?</ac:rich-text-body></ac:structured-macro><p />"
-    )
-    weekly_update = re.match(weekly_update_pattern, content)
-    if weekly_update:
-        return form_update_dict(weekly_update.group())
-    return {}
+    update_dict = {}
+    soup = BeautifulSoup(raw_content, "html.parser")
+
+    date_heading = soup.find("h2", string=date)
+
+    for subteam in SUBTEAMS:
+        update_dict[subteam] = {}
+        # Find subteam update
+        subteam_heading = date_heading.find_next(string=subteam)
+        if subteam_heading:
+            for category_heading in UPDATE_CATEGORIES:
+                # Find R (results from last week's goals) and TWG (this week's goals)
+                section = subteam_heading.find_next(
+                    "p", string=lambda text: text and text.strip() == category_heading
+                )
+                if section:
+                    update_dict[subteam][category_heading] = extract_list_items(
+                        section.find_next("ul")
+                    )
+
+    return update_dict
 
 
 def fetch_content(date: str):
@@ -80,6 +92,6 @@ def fetch_content(date: str):
     if response.status_code == 200:
         page_content = response.json()
         body = page_content.get("body", {}).get("storage", {}).get("value")
-        return parse_content(content=body, date=date)
+        return parse_content(raw_content=body, date=date)
     print(f"Failed to retrieve page. Status code: {response.status_code}")
     return {}
